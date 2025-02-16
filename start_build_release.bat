65adf79e6fa28887a7e36a033ff9f80d974a9d71
poto <potoshop10@yandex.ru>
2025-02-16 11:04:56 +0300

    add target to Release

--- start_build_release.bat
+++ start_build_release.bat
@@ -1,5 +1,5 @@
 call conan install . --output-folder=build --profile=profile_release --profile:b=profile_release
 call cd build
 call cmake ..  -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake --fresh
-call cmake --build . --config Release
+call cmake --build . --target RedmineHelper_imGui --config Release
 call cd ..
